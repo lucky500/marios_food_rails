87a33ddac070e92e1acbc6365251f2e77e40f4e0
Luciano Oliveira <luciano101@msn.com>
2017-11-17 11:51:28 -0800

    add edit route and find_product private method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,16 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :udpate, :destroy]
 
   def index
+    @products = Product.all
+  end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :list
+  resources :products
   root 'products#index'
 end
