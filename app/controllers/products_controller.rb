d856250721fe22a973eebb1dc14fe0aebe98db72
Luciano Oliveira <luciano101@msn.com>
2017-11-17 12:47:04 -0800

    add edit and create methods to controller along with views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :find_product, only: [:show, :edit, :udpate, :destroy]
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all
@@ -9,7 +9,35 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+    @product  = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
   private
+  def product_params
+    params.require(:product).permit(:name, :cost, :country_of_origin)
+  end
+
   def find_product
     @product = Product.find(params[:id])
   end
